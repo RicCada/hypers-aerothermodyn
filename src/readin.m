5797eb587688064f346bc6f5d910d394525c00f4
RicCada <riccardo.cadamuro@skywarder.eu>
2024-01-21 14:52:45 +0100

    Update path [readin]

--- src/readin.m
+++ src/readin.m
@@ -3,9 +3,9 @@ function [vehicle] = readin(objName)
 % such as reference area, reference length, and CG location in mm and mm^2
 % for models in the database
 
-vehicle.mesh = stlread(strcat(objName, '.stl'));
+vehicle.mesh = stlread(strcat('data\',objName,'\',objName,'.stl'));
 
-fID = fopen(strcat(objName, '.txt'),'r');
+fID = fopen(strcat('data\',objName,'\',objName,'.txt'),'r');
 data = fscanf(fID, '%f');
 
 vehicle.data.SRef = data(1);
