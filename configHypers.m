9f336d4c988a4dc513e747409c021bcd466a9318
RicCada <riccardo.cadamuro@skywarder.eu>
2024-02-04 19:26:58 +0100

    Test config for high atmosphere

--- configHypers.m
+++ configHypers.m
@@ -1,15 +1,20 @@
 function data = configHypers()
     KM = 1000; 
-
+    RE = 6.3781e6; 
 
     % Select vehicle
-    data.vehicleName = 'ApolloCapsule'; 
+    data.vehicleName = 'Plug'; 
 
     % Select flight conditionss
-    data.fltcon.alpha = (26.1) *pi/180; 
-    data.fltcon.alt = [75] .* KM; 
-    [~,vSon,~,~] = atmoscoesa(data.fltcon.alt);
+    data.fltcon.alpha = (-180:5:180) *pi/180; 
+    data.fltcon.alt = [105] .* KM; 
+        
+    hGP = data.fltcon.alt/(1 + data.fltcon.alt/RE); 
+    T = atmoscira(0, 'GPHeight', hGP); 
+    vSon = sqrt(1.4 * 287.05 * T); 
+
     data.fltcon.mach = (9600/vSon);
+
 
     data.fltcon.gamma = 1.4;
 
